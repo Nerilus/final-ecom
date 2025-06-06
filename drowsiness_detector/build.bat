e7251bc4e915e2b94e52171c7b5f8449881b74b9
Daphné Nerilus <daphnenerilus@MacBook-Air-de-Daphne.local>
2025-06-06 09:59:15 +0200

    update bat

--- drowsiness_detector/build.bat
+++ drowsiness_detector/build.bat
@@ -1,7 +1,16 @@
 @echo off
 echo Installation de l'environnement de build...
+
+:: Suppression de l'ancien environnement virtuel s'il existe
+if exist venv rmdir /s /q venv
+
+:: Création d'un nouvel environnement virtuel
 python -m venv venv
 call venv\Scripts\activate.bat
+
+:: Mise à jour de pip et installation des dépendances de base
+python -m pip install --upgrade pip
+pip install setuptools wheel
 
 echo Installation des dependances...
 pip install -r requirements-build.txt
